040a47472b3cff3b6b8d67bb43d192d048a1434a
Huansha13 <ing.yfhl13@outlook.com>
2021-05-07 23:24:14 -0500

    blog ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCOJO4Cv4EuEFRquylbIYOvx068Llsj96U',
+    authDomain: 'blog-b1ce6.firebaseapp.com',
+    databaseURL: 'https://blog-b1ce6-default-rtdb.firebaseio.com',
+    projectId: 'blog-b1ce6',
+    storageBucket: 'blog-b1ce6.appspot.com',
+    messagingSenderId: '498015475872',
+    appId: '1:498015475872:web:bac4e04b3b33d9d62951c5'
+  }
 };
